6248ef2facdedaa9c3e905ff8fdde32d09217545
liamchampton <liam.hampton@hotmail.co.uk>
2021-03-08 14:36:37 +0000

    build: Initial commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/go-qr
+
+go 1.16
+
+require (
+	github.com/divan/qrlogo v1.0.2
+	github.com/gorilla/mux v1.8.0
+)
---
+++ main.go
@@ -0,0 +1,140 @@
+package main
+
+import (
+	"bytes"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"image"
+	"image/png"
+	"io/ioutil"
+	"log"
+	"math/rand"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/divan/qrlogo"
+	"github.com/gorilla/mux"
+)
+
+const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+var seededRand *rand.Rand = rand.New(rand.NewSource(time.Now().UnixNano()))
+
+func main() {
+	gracefulTerminateSystem()
+
+	var dir string = "html"
+	r := mux.NewRouter()
+
+	r.PathPrefix("/html/").Handler(http.StripPrefix("/html/", http.FileServer(http.Dir(dir)))).Methods("GET")
+	r.HandleFunc("/", home).Methods("GET")
+	r.HandleFunc("/create-qrcode", createQrCode).Methods("POST")
+
+	srv := &http.Server{
+		Handler:      r,
+		Addr:         "localhost:8080",
+		WriteTimeout: 30 * time.Second,
+		ReadTimeout:  30 * time.Second,
+	}
+
+	currentTime := time.Now()
+	fmt.Println("QR Code Generator server is started...", currentTime.Format("Mon 02 Jan 2006 03:04pm"))
+	log.Fatal(srv.ListenAndServe())
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	filename := "./html/index.html"
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Fprint(w, err)
+	}
+
+	fmt.Fprintf(w, string(body))
+}
+
+func createQrCode(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method != http.MethodPost {
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
+
+	imgBase64 := r.FormValue("image")
+	url := r.FormValue("url")
+
+	imgBase64cleaned := imgBase64[len("data:image/png;base64,"):len(imgBase64)]
+
+	imgBytes, _ := base64.StdEncoding.DecodeString(imgBase64cleaned)
+
+	img, _, _ := image.Decode(bytes.NewReader(imgBytes))
+
+	fname := RandomString(16)
+	currentDir, _ := os.Getwd()
+	logoFile := currentDir + "/html/uploaded_logos/" + fname + ".png"
+	qrCodeFile := currentDir + "/html/qr_codes/" + fname + ".png"
+	imgFile, err := os.Create(logoFile)
+	if err != nil {
+		panic(err)
+	}
+	png.Encode(imgFile, img)
+
+	file, err := os.Open(logoFile)
+	errcheck(err, "Failed to open logo:")
+	defer file.Close()
+
+	logo, _, err := image.Decode(file)
+	errcheck(err, "Failed to decode PNG with logo:")
+
+	qr, err := qrlogo.Encode(url, logo, 2000)
+	errcheck(err, "Failed to encode QR:")
+
+	out, err := os.Create(qrCodeFile)
+	errcheck(err, "Failed to open output file:")
+	out.Write(qr.Bytes())
+	out.Close()
+
+	writeResponse(w, http.StatusCreated, map[string]string{"fname": fname + ".png"})
+
+	fmt.Println("QR image generated: ", fname+".png")
+}
+
+func StringWithCharset(length int, charset string) string {
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = charset[seededRand.Intn(len(charset))]
+	}
+	return string(b)
+}
+
+func RandomString(length int) string {
+	return StringWithCharset(length, charset)
+}
+
+func writeResponse(w http.ResponseWriter, code int, message map[string]string) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+
+	var jsonData []byte
+	jsonData, _ = json.Marshal(message)
+	fmt.Fprint(w, string(jsonData))
+}
+
+func errcheck(err error, str string) {
+	if err != nil {
+		fmt.Println(err, str)
+		os.Exit(1)
+	}
+}
+func gracefulTerminateSystem() {
+	c := make(chan os.Signal, 2)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-c
+		fmt.Println("\r- Ctrl+C pressed in Terminal")
+		os.Exit(0)
+	}()
+}
